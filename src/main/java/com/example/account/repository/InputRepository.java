fc47c9102024eb8ef15cab1bb7e64bf267bcf256
Inyour97 <pg11031@naver.com>
2025-07-19 09:24:42 +0900

    내용 조회 수정

--- src/main/java/com/example/account/repository/InputRepository.java
+++ src/main/java/com/example/account/repository/InputRepository.java
@@ -3,5 +3,9 @@ package com.example.account.repository;
 import com.example.account.entity.InputDetail;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface InputRepository extends CrudRepository<InputDetail,Long> {
+    @Override
+    Iterable<InputDetail> findAll();
 }
